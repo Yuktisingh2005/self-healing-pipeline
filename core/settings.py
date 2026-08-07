ec578794c20e09119bbcddb7155e63ec2385ea35
Yuktisingh2005 <yuktisingh2005@gmail.com>
2026-08-07 15:35:20 +0530

    Fix: allow all hosts for internal container-to-container health checks

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-d1$jv98sdh@f0(=vxlzqblx-*7*e94mchf$qhnbjvwi$yf*azj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
